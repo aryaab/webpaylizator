bfafc5c8238b0a4cb2c93a6aefdb213549e9543a
mathias <mathiasd88@gmail.com>
2016-01-19 01:00:47 -0300

    First commit

---
+++ src/WebPaylizatorServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace mathiasd88\webpaylizator;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebPaylizatorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // TODO:: Implement boot() method.
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
